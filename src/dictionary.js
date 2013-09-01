1c674967b078faf3bea89fc68a386f03c376c1a2
Yehonatan Daniv <maggotfish@gmail.com>
2013-09-01 11:27:40 +0300

    Initial commit of index.html, package.json, main.js,  dictionary.js

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,5 +1,45 @@
-define(function () {
+define(function() {
     return {
-        // put your i18n dictionary here
+        'document-title'        : gettext('Open Muni Budget | Budget Comparisons Visualization'),
+        enter                   : gettext('Enter'),
+        'welcome-message'       : gettext('Welcome to Budget Comparisons 5000'),
+        'choose-entity'         : gettext('Choose entity'),
+        'search-placeholder'    : gettext('Search'),
+        'choose-budget-item'    : gettext('Choose budget items:'),
+        'search-under'          : gettext('Search under:'),
+        main                    : gettext('Main'),
+        code                    : gettext('Code'),
+        name                    : gettext('Name'),
+        type                    : gettext('Type'),
+        done                    : gettext('Done'),
+        add                     : gettext('Add'),
+        'site-menu-home'        : gettext('Home'),
+        'site-menu-munis'       : gettext('Munis'),
+        'site-menu-projects'    : gettext('Projects'),
+        'site-menu-about'       : gettext('About'),
+        'site-menu-api'         : gettext('API'),
+        'site-menu-tos'         : gettext('Terms'),
+        'site-menu-credits'     : gettext('Credits'),
+        loading                 : gettext('Loading...'),
+        'open-muni-budget'      : gettext('Open Muni Budget'),
+        compare                 : gettext('Compare'),
+        'viz-new'               : gettext('New'),
+        'viz-duplicate'         : gettext('Duplicate'),
+        'viz-delete'            : gettext('Delete'),
+        'viz-export'            : gettext('Export'),
+        'viz-publish'           : gettext('Publish'),
+        'viz-save'              : gettext('Save'),
+        'search-menu-free'      : gettext('Free text search'),
+        'search-term-prefix'    : gettext('Search for'),
+        'guest'                 : gettext('Guest'),
+        'summarize-data'        : gettext('Summarize data of:'),
+        'budget'                : gettext('Budget'),
+        'actual'                : gettext('Actual'),
+        'selected-items'        : gettext('selected items'),
+        'normalize-data'        : gettext('Normalize data'),
+        'no-normalization'      : gettext("Don't normalize"),
+        'population'            : gettext('Population'),
+        'ground-surface'        : gettext('Ground Surface')
     };
+
 });
--- src/main.js
+++ src/main.js
@@ -1,14 +1,41 @@
 requirejs.config({
-    baseUrl : '/src',
+    baseUrl : '/static/lib',
     paths   : {
-        api         : 'http://dev.openmuni.org.il/static/src/api',
-        i18n        : 'http://dev.openmuni.org.il/static/src/i18n',
-        dictionary  : 'dictionary'
+        jqscroll            : 'jqScroll/jqscroll',
+        'jquery.mousewheel' : 'jquery-mousewheel',
+        uijet_dir           : 'uijet',
+        widgets             : 'uijet/widgets',
+        composites          : 'uijet/composites',
+        modules             : 'uijet/modules',
+        comparisons            : '../projects/comparisons/comparisons',
+        ui                  : '../projects/comparisons/ui',
+        resources           : '../projects/comparisons/resources',
+        controllers         : '../projects/comparisons/controllers',
+        project_modules     : '../projects/comparisons/modules',
+        project_widgets     : '../projects/comparisons/widgets',
+        project_mixins      : '../projects/comparisons/mixins',
+        dictionary          : '../projects/comparisons/dictionary',
+        api                 : '../src/api',
+        i18n                : '../src/i18n'
+    },
+    shim    : {
+        eventbox                : ['setImmediate'],
+        'backbone-fetch-cache'  : 'modules/data/backbone'
     }
 });
 requirejs([
-    'app' // name your app
-], function (app) {
+    'ui/main',
+    '../js/base'
+], function (comparisons) {
 
-    // start doing stuff
+    comparisons.start({
+        AUTH_URL: window.AUTH_URL,
+        auth    : {
+            client_id       : '751be246011e8a6198d7',
+            client_secret   : 'c62cb3b66fcbe46b82ecda2ed146b7bfe24fdea4',
+            grant_type      : 'password',
+            username        : 'admin',
+            password        : 'morelove!'
+        }
+    });
 });
